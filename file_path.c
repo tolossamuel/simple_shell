0c7d14409baf0837cc7e914ab0a483c49f26b07b
tolossamuel <tolossamuel1@gmail.com>
2023-09-20 00:35:00 +0300

    betty

--- file_path.c
+++ file_path.c
@@ -1,46 +1,58 @@
 #include "shell.h"
 
+/**
+ * file_path - searches for an environment variable named
+ * @name: const char for the functions
+ * Return: NULL if not name else its value of char in functions
+*/
 char *file_path(const char *name)
 {
-    int i, j;
-    char *value;
+	int num1, num2;
+	char *value;
 
-    if (!name)
-        return (NULL);
-    for (i = 0; environ[i]; i++)
-    {
-        j = 0;
-        if (name[j] == environ[i][j])
-        {
-            while (name[j])
-            {
-                if (name[j] != environ[i][j])
-                    break;
+	if (!name)
+		return (NULL);
+	for (num1 = 0; environ[num1]; num1++)
+	{
+		num2 = 0;
+		if (name[num2] == environ[num1][num2])
+		{
+			while (name[num2])
+			{
+				if (name[num2] != environ[num1][num2])
+					break;
 
-                j++;
-            }
-            if (name[j] == '\0')
-            {
-                value = (environ[i] + j + 1);
-                return (value);
-            }
-        }
-    }
-    return (NULL); 
+				num2++;
+			}
+			if (name[num2] == '\0')
+			{
+				value = (environ[num1] + num2 + 1);
+				return (value);
+			}
+		}
+	}
+	return (NULL);
 }
 
+/**
+ * search_file_path - attempts to find the full path of a file
+ * named filename within the directories specified in the arr array
+ * @filename: the name of the file to search for in the directories
+ * specified in arr
+ * @arr: an array of strings that represents a list of directories
+ * Return: the full path of a file if found, or NULL if the file
+ * is not found in the specified directories
+*/
 char *search_file_path(char *filename, char **arr)
 {
-	
+
 	char *string;
 	int num1 = 0;
 	struct stat structure;
 
 	while (arr[num1])
 	{
-	
 		string = string_repetitions(arr[num1]);
-
 		string = string_merging(string, "/");
 		string = string_merging(string, filename);
 		if (stat(string, &structure) == 0)
@@ -50,6 +62,5 @@ char *search_file_path(char *filename, char **arr)
 		free(string);
 		num1++;
 	}
-
 	return (NULL);
 }
